a057803ab78d755be746e6002f7711137094f09d
Archan Rajaram R <archan.rajaram1@gmail.com>
2018-03-14 12:24:21 +0530

    Initial commit

---
+++ app/src/main/java/com/hackathon/cyberblue/crimeagainstwomenapp/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.hackathon.cyberblue.crimeagainstwomenapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Intent i = new Intent(SplashScreen.this,MainActivity.class);
+        startActivity(i);
+    }
+}
